b71ea30f83e5f93f32cf9852fbf092b2af002090
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-06-05 12:35:43 -0300

    feat: alter config

--- quasar.config.js
+++ quasar.config.js
@@ -58,7 +58,7 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16'
       },
 
-      vueRouterMode: 'hash' // available values: 'hash', 'history'
+      vueRouterMode: 'history' // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -85,6 +85,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
+      port: 8080,
       open: true // opens browser window automatically
     },
 
