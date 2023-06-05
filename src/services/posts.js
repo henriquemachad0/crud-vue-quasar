098298aecbec66e1b4574ea822884894831df2cb
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-06-05 18:15:13 -0300

    feat: add getById in services

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,12 +1,13 @@
 import useApi from 'src/composables/UseApi'
 
 export default function postService () {
-  const { list, post, update, remove } = useApi('posts')
+  const { list, getById, post, update, remove } = useApi('posts')
 
   return {
     list,
     post,
     update,
-    remove
+    remove,
+    getById
   }
 }
